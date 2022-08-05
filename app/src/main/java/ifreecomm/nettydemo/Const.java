57d4b77015bc5a379ce5f7001d20cf9c9fe543fb
mingxing <icediv@163.com>
2022-08-05 14:15:01 +0800

    update port to 8888;change single server instance method;added more info for server gradle android config

--- app/src/main/java/ifreecomm/nettydemo/Const.java
+++ app/src/main/java/ifreecomm/nettydemo/Const.java
@@ -7,5 +7,5 @@ package ifreecomm.nettydemo;
 public class Const {
 //    public static final String HOST = "192.168.232.2";
     public static final String HOST = "10.0.2.2";
-    public static final int TCP_PORT = 1088;
+    public static final int TCP_PORT = 8888;
 }
